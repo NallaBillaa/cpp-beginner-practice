28131663a4e681310f3da3cfe8161ef3820ee098
NallaBillaa <abhasyadav3024@gmail.com>
2026-09-02 11:03:16 +0000

    maybe right

---
+++ Basics/calculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    char choice;
+    double a,b;
+
+    cout << "Enter your first number: ";
+    cin >> a;
+    cout << "Enter your first number: ";
+    cin >> b;
+    cout << "\nWhich operation do you want:\n?";
+    cout << "a) Addition\n?"; 
+    cout << "b) Subtraction\n?";
+    cout << "c) Product\n";
+    cout << "d) Quotient\n";
+    cout << "e) Remainder\n";
+    cout << "\nEnter your choice";
+    cin >> choice;
+
+    if (choice == 'a'){
+        cout << "Result:" << a + b;
+    }
+    else if (choice == 'b'){
+        cout << "Result:" << a - b;
+    }
+    else if (choice == 'c'){
+        cout << "Result:" << a * b;
+    }
+    else if (choice == 'd'){
+        if (b=!0){
+        cout << "Result:" << a / b;
+        }
+        else {
+            cout << "Cannot divide by zero";
+        }
+    }
+    else if (choice == 'e'){
+        cout << "Result:" << a % b;
+    }
+    else {
+        cout << "Invalid Choice";
+    }
+}
